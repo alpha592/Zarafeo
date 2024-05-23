496401c28fa3d127d7c1bb858a47cb5e8900c7db
TokyNantenaina1706 <tokyrandriamalalanirina@gmail.com>
2024-05-23 10:08:48 +0300

    Modification du fichier int.py pour namboariko kely ftsn le code

--- int.py
+++ int.py
@@ -1,31 +1,30 @@
-from customtkinter import *
+from customtkinter import CTk, CTkButton, CTkLabel, CTkToplevel, set_appearance_mode
 from customtkinter import filedialog
-import customtkinter as ctk
+
 def ouvrir_repertoire():
     filedialog.askdirectory()
+
 def nouvelle_fenetre():
-    nouv=CTk()
-    nouvelle=ctk.CTkToplevel(app)
+    nouvelle = CTkToplevel(app)
     nouvelle.geometry("1080x1080")
-    nouvelle.title("separation")
-    nouvelle._raise_(app)
-    imp=CTkButton(text="importer",corner_radius=32,width=300,height=60,text_color="white")
-    imp.place(relx=0.4,rely=0.4,anchor="center")
-app.close
-app= CTk()
+    nouvelle.title("Séparation")
+    imp = CTkButton(master=nouvelle, text="Importer", corner_radius=32, width=300, height=60, text_color="white")
+    imp.place(relx=0.4, rely=0.4, anchor="center")
 
+app = CTk()
 app.geometry("1080x1080")
-
-set_appearance_mode("dark")
 app.title("Zarafeo")
-btn=CTkButton(master=app,text="karaoke",corner_radius=32,fg_color="#059C02",hover_color="#C850C0",command=ouvrir_repertoire,width=250,height=50,text_color="white",font=("courrier",20))
-btn1=CTkButton(master=app,text="Separation",corner_radius=32,fg_color="#059C02",width=250,height=50,text_color="white",command=nouvelle_fenetre,font=("courrier",20))
-btn.place(relx=0.66,rely=0.4,anchor="center")
-btn1.place(relx=0.33,rely=0.4,anchor="center")
-titre=CTkLabel(master=app,text="Bienvenue sur Zarafeo",font=("Poppins",70),)
-titre.place(relx=0.5,rely=0.1,anchor="center")
-soratra=CTkLabel(master=app,text="SEPARATION   VOCAL &  INSTRUMENTAL +  KARAOKE",font=("poppins",12),text_color="#8C53FF")
-soratra.place(relx=0.36,rely=0.7)
+set_appearance_mode("white")
 
-#059C02
+btn = CTkButton(master=app, text="Karaoke", corner_radius=32, fg_color="#059C02", hover_color="#C850C0", command=ouvrir_repertoire, width=250, height=50, text_color="white", font=("courier", 20))
+btn1 = CTkButton(master=app, text="Séparation", corner_radius=32, fg_color="#059C02", width=250, height=50, text_color="white", command=nouvelle_fenetre, font=("courier", 20))
+btn.place(relx=0.66, rely=0.4, anchor="center")
+btn1.place(relx=0.33, rely=0.4, anchor="center")
+
+titre = CTkLabel(master=app, text="Bienvenue sur Zarafeo", font=("Poppins", 70))
+titre.place(relx=0.5, rely=0.1, anchor="center")
+
+soratra = CTkLabel(master=app, text="Séparation Vocal & Instrumental + Karaoke", font=("poppins", 12), text_color="#8C53FF")
+soratra.place(relx=0.36, rely=0.7)
+
 app.mainloop()
